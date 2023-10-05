1c9e5bee0f6737be90f2a396bb53575bd5d2c94e
hulkvenom <115841729+hulkvenom@users.noreply.github.com>
2023-10-04 22:29:18 -0500

    map

---
+++ Pac-Man/Assets/_MyFiles/Scripts/SpawnNodes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class SpawnNodes : MonoBehaviour
+{
+    int numToSpawn = 28;
+
+    public float currentSpawnOffset;
+    public float spawnOffest = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
