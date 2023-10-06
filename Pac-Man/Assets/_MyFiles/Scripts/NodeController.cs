613ada4e6eb1d0a2becd13f8efc96d2a932322f8
hulkvenom <115841729+hulkvenom@users.noreply.github.com>
2023-10-06 16:10:25 -0500

    pacman enter

---
+++ Pac-Man/Assets/_MyFiles/Scripts/NodeController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class NodeController : MonoBehaviour
+{
+
+    public bool canMoveLeft = false;
+    public bool canMoveRight = false;
+    public bool canMoveUp = false;
+    public bool canMoveDown = false;
+
+    public GameObject nodeLeft;
+    public GameObject nodeRight;
+    public GameObject nodeUp;
+    public GameObject nodeDown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaycastHit2D[] hitsDown;
+        //Shoot rsycast (line) going down
+        hitsDown = Physics2D.RaycastAll(transform.position, -Vector2.up);
+
+        //Loop through all of the gameobjects that the raycast hits
+        for (int i = 0; i < hitsDown.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsDown[i].point.y - transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveDown = true;
+                nodeDown = hitsDown[i].collider.gameObject;
+            }
+        }
+
+        RaycastHit2D[] hitsUp;
+        //Shoot raycast (line) going up
+        hitsUp = Physics2D.RaycastAll(transform.position, Vector2.up);
+
+        //Loop through all of the gameobjects that the raycast hits
+        for (int i = 0; i < hitsUp.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsUp[i].point.y - transform.position.y);
+            if (distance < 0.4f)
+            {
+                canMoveUp = true;
+                nodeUp = hitsUp[i].collider.gameObject;
+            }
+        }
+
+        RaycastHit2D[] hitsRight;
+        //Shoot raycast (line) going right
+        hitsRight = Physics2D.RaycastAll(transform.position, Vector2.right);
+
+        //Loop through all of the gameobjects that the raycast hits
+        for (int i = 0; i < hitsRight.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsRight[i].point.x - transform.position.x);
+            if (distance < 0.4f)
+            {
+                canMoveRight = true;
+                nodeRight = hitsRight[i].collider.gameObject;
+            }
+        }
+
+        RaycastHit2D[] hitsLeft;
+        //Shoot raycast (line) going left
+        hitsLeft = Physics2D.RaycastAll(transform.position, -Vector2.right);
+
+        //Loop through all of the gameobjects that the raycast hits
+        for (int i = 0; i < hitsLeft.Length; i++)
+        {
+            float distance = Mathf.Abs(hitsLeft[i].point.x - transform.position.x);
+            if (distance < 0.4f)
+            {
+                canMoveLeft = true;
+                nodeLeft = hitsLeft[i].collider.gameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetNodeFromDirection(string direction)
+    {
+        if (direction == "left" && canMoveLeft)
+        {
+            return nodeLeft;
+        }
+        else if (direction == "right" && canMoveRight)
+        {
+            return nodeRight;
+        }
+        else if (direction == "up" && canMoveUp)
+        {
+            return nodeUp;
+        }
+        else if (direction == "down" && canMoveDown)
+        {
+            return nodeDown;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
